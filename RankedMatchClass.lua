364a350123879f158cb3c795339bacfaf9942d1c
BloodSpectre <90489393+BloodSpectre@users.noreply.github.com>
2021-10-14 01:55:34 +0100

    Update RankedMatchClass.lua

--- RankedMatchClass.lua
+++ RankedMatchClass.lua
@@ -5,6 +5,18 @@
 
 --//USE OF METATABLES
 
+--[[
+	
+	// STILL TO DO:
+	
+	// - HANDLE PLAYER LEAVING
+	// - USER INTERFACES
+	// - SPECIAL EXCEPTIONS
+	
+	// - MAIN MATCHMAKING SERVICE REWORK
+
+]]
+
 --//Predefines
 local teams = game:GetService("Teams")
 local lobby = game.Teams.Lobby
@@ -24,6 +36,9 @@ class.MatchOngoing = false
 class.Winner = "UNDECIDED"
 class.Round = 0
 class.MatchID = 0
+
+class.Player1Spawn = game.Workspace.Arena.Player1Spawn.CFrame
+class.Player2Spawn = game.Workspace.Arena.Player2Spawn.CFrame
 
 function class:getAttributes()
 	print(string.format("MATCH: %s", self.MatchID))
@@ -50,25 +65,35 @@ end
 
 function resetMatch(self)
 	
-	wait(2) -- // wait for respawn
+	wait(1) -- // wait for respawn
 	
 	self.Player1.Character.Humanoid.Health=self.Player1.Character.Humanoid.MaxHealth
 	self.Player2.Character.Humanoid.Health=self.Player2.Character.Humanoid.MaxHealth
 	
-	--//* TELEPORT PLAYERS *//
+	self.Player1.Character.HumanoidRootPart.CFrame = self.Player1Spawn
+	self.Player2.Character.HumanoidRootPart.CFrame = self.Player2Spawn
+	
 end
 
 function giveStats(self)
-	--//* AWARD WINS / LOSSES *//
+	if self.Winner == self.Player1 then
+		game.ServerStorage[".Ranked"][self.Player1.Name].RankedWins.Value += 1
+	elseif self.Winner == self.Player2 then
+		game.ServerStorage[".Ranked"][self.Player2.Name].RankedWins.Value += 1
+	end
+	print("Awarded Stats")
 end
 
 function endMatch(self)
-	wait(3) -- // Await character respawn
-	pcall(function()
+	wait(3.5) -- // Await character respawn
+	local success,errormsg = pcall(function()
 		self.Player1.Character.Humanoid.Health=0
 		self.Player2.Character.Humanoid.Health=0
 		--// Reset Arena
 	end)
+	if not success then
+		warn(errormsg)
+	end
 end
 
 function class:Initiate()
@@ -81,6 +106,8 @@ function class:Initiate()
 	
 	wait(3) -- // wait for char spawning
 	
+	resetMatch(self)
+	
 	self.Player1.Character.Humanoid.Died:Connect(function()
 		if self.MatchOngoing == true then
 			print("PLAYER 1 DIED, INCREASING PLAYER 2 SCORE")
@@ -91,9 +118,11 @@ function class:Initiate()
 	self.Player1.CharacterAdded:Connect(function()
 		if self.MatchOngoing == true then
 			self.Player1.Character.Humanoid.Died:Connect(function()
+				if self.MatchOngoing == true then
 					print("PLAYER 1 DIED, INCREASING PLAYER 2 SCORE")
 					self.Player2Score = self.Player2Score + 1
 					resetMatch(self)
+				end
 			end)
 		end
 	end)
@@ -108,27 +137,33 @@ function class:Initiate()
 	self.Player2.CharacterAdded:Connect(function()
 		if self.MatchOngoing == true then
 			self.Player2.Character.Humanoid.Died:Connect(function()
-				print("PLAYER 2 DIED, INCREASING PLAYER 1 SCORE")
-				self.Player1Score = self.Player1Score + 1
-				resetMatch(self)
+				if self.MatchOngoing == true then
+					print("PLAYER 2 DIED, INCREASING PLAYER 1 SCORE")
+					self.Player1Score = self.Player1Score + 1
+					resetMatch(self)
+				end
 			end)
 		end
 	end)
 	
 	-- // ASYNC Score checker
 	
-	scorechecker = coroutine.create(function()
+	local scorechecker = coroutine.create(function()
 		while wait(1) do
 			--print("RUNNING SCORE CHECK")
-			if self.Player1Score == self.RoundWin then
-				print(self.Player1.Name.." has won the match!")
-				self.Winner = self.Player1
-				self:Stop(true)
-			end
-			if self.Player2Score == self.RoundWin then
-				print(self.Player2.Name.." has won the match!")
-				self.Winner = self.Player2
-				self:Stop(true)
+			if self.MatchOngoing == true then
+				if self.Player1Score == self.RoundWin then
+					print(self.Player1.Name.." has won the match!")
+					self.Winner = self.Player1
+					self:Stop(true)
+				end
+				if self.Player2Score == self.RoundWin then
+					print(self.Player2.Name.." has won the match!")
+					self.Winner = self.Player2
+					self:Stop(true)
+				end
+			else
+				coroutine.yield()
 			end
 		end
 	end)
@@ -142,16 +177,13 @@ end
 function class:Stop(awardWinner)
 	print("STOPPING MATCH "..tostring(self.MatchID))
 	self.MatchOngoing = false
+	endMatch(self)
 	if awardWinner == true then
 		giveStats(self)
 	end
-	coroutine.yield(scorechecker)
-	endMatch(self)
-	spawn(function()
-		wait(5)
-		print("MATCH DELETED: "..self.MatchID)
-		self = nil
-	end)
+	wait(3)
+	print("MATCH DELETED: "..self.MatchID)
+	self = nil
 end
 
 
